e0f97d7e15b34ef1e1b0858988b40fc42db06fd0
Bruno Serpa <brunospenna@gmail.com>
2022-08-31 12:49:35 -0300

    boarde em gestão de estado

--- src/index.js
+++ src/index.js
@@ -6,28 +6,36 @@ import './index.css';
 
 class Square extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          value: null
-        };
-    }
-
     render() {
         return (
-            <button className="square" onClick={ () => { 
-                    console.log("click")
-                    this.setState({value: 'X'}) 
-                }}>
-                {this.state.value}
+            <button className="square" 
+            onClick={() => {this.props.onClick() }}> 
+                      
+               
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+      }
+      handleClick(i) {
+         const squares = this.state.squares.slice();
+         squares[i] ='X'
+         this.setState({squares: squares});
+      }
     renderSquare(i) {
-        return <Square value={i} />;
+       return <Square 
+       value={this.state.squares[i]}
+       onClick={() =>{this.handleClick(i)}}
+       />;
+      
     }
 
     render() {
